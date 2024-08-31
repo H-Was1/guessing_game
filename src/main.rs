18ba7b8f45a9178ffd4b5522446d76a5e1cc6ef1
wasi <hammadwasi.b@gmail.com>
2024-08-31 20:36:49 +0500

    keep asking

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,17 @@ use std::io;
 fn main() {
     println!("Starting the 'Guessing Game'...!");
     println!("Guess the number");
+    let mut res = 0;
+    while res.clone() < 1 {
+        // if answer is wrong then keep res=0 and if answer right then res=1
+        comparer(&mut res);
+    }
+}
+
+fn comparer(res: &mut i32) {
     let mut guess: String = String::new();
     let numb = rand::thread_rng().gen_range(1..=10);
+
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read user input");
@@ -14,6 +23,9 @@ fn main() {
     match guess_number.cmp(&numb) {
         Ordering::Less => println!("Too low! Try again. {numb}"),
         Ordering::Greater => println!("Too high! Try again. {numb}"),
-        Ordering::Equal => println!("Congratulations! You guessed the correct number. {numb}"),
+        Ordering::Equal => {
+            println!("Congratulations! You guessed the correct number.");
+            *res = 1;
+        }
     }
 }
