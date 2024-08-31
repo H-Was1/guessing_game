2da68f666d3ae32ace4e492e898c9e3b98838b61
wasi <hammadwasi.b@gmail.com>
2024-08-31 20:16:05 +0500

    Finish

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 fn main() {
     println!("Starting the 'Guessing Game'...!");
     println!("Guess the number");
     let mut guess: String = String::new();
-    let numb = rand::thread_rng().gen_range(1..10);
+    let numb = rand::thread_rng().gen_range(1..=10);
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read user input");
-    println!("{}", numb);
-    // if &guess.parse::<i8>().expect("Something went wrong") > &numb {
-    //     println!("Too high! Try again.");
-    // } else if &guess.parse::<i8>().expect("Something went wrong") < &numb {
-    //     println!("Too low! Try again.");
-    // } else if &guess.parse::<i8>().expect("Something went wrong") == &numb {
-    //     println!("Congratulations! You guessed the correct number.");
-    // }
+    // println!("{}", numb);
+    let guess_number = guess.trim().parse::<i8>().expect("Failed to parse");
+    match guess_number.cmp(&numb) {
+        Ordering::Less => println!("Too low! Try again. {numb}"),
+        Ordering::Greater => println!("Too high! Try again. {numb}"),
+        Ordering::Equal => println!("Congratulations! You guessed the correct number. {numb}"),
+    }
 }
