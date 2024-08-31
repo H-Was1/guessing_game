c9e70ef32530895fe788eac6989bbc2181190d0a
wasi <hammadwasi.b@gmail.com>
2024-08-31 19:08:25 +0500

    init

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
+use rand::Rng;
 use std::io;
 fn main() {
     println!("Starting the 'Guessing Game'...!");
     println!("Guess the number");
+    let mut guess: String = String::new();
+    let numb = rand::thread_rng().gen_range(1..10);
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read user input");
+    println!("{}", numb);
+    // if &guess.parse::<i8>().expect("Something went wrong") > &numb {
+    //     println!("Too high! Try again.");
+    // } else if &guess.parse::<i8>().expect("Something went wrong") < &numb {
+    //     println!("Too low! Try again.");
+    // } else if &guess.parse::<i8>().expect("Something went wrong") == &numb {
+    //     println!("Congratulations! You guessed the correct number.");
+    // }
 }
